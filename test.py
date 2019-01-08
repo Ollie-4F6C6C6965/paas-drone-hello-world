f1ce44ba277827abcf9395418b0a3a179b48f99f
Ollie Chalk <oliver.chalk@digital.cabinet-office.gov.uk>
2019-01-08 14:25:48 +0000

    TEST_ENV_ENC assertion

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ def main():
     print("test.py:main:", os.getenv('TEST_ENV_NOTEXISTS', default_value))
     print("test.py:main:", os.getenv('TEST_ENV_PLAIN', default_value))
     print("test.py:main:", os.getenv('TEST_ENV_ENC', default_value))
+    print("test.py:main: TEST_ENV_ENC is as expected:", os.getenv('TEST_ENV_ENC', default_value) == "TestSecret!")
 
 main()
